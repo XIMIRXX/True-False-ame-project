6e1b636c8335a153562fac6727a5cbd4375f57df
XIMIRXX <imirbadirli466@gmail.com>
2026-06-03 14:07:03 +0300

    Add comment for file dependencies
    Added a comment to ensure required files are in the same directory.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Make sure data.py, quiz_brain.py and question_model.py are in the same directory
+
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
